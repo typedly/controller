25f8a6f474f18dce7939dcad8c151e0d57d84618
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-15 07:52:58 +0000

    refactor(`PropertyControllerShape`): use `DescriptorsShape` and change params to `index` from `id`.

--- src/property/lib/property-controller-shape.interface.ts
+++ src/property/lib/property-controller-shape.interface.ts
@@ -1,5 +1,5 @@
 // Interface.
-import { PropertyDescriptorChainShape, WrappedPropertyDescriptor } from '@typedly/descriptor';
+import { DescriptorsShape, PropertyDescriptorChainShape, WrappedPropertyDescriptor } from '@typedly/descriptor';
 // Type.
 import { GetterCallback } from '@typedly/callback';
 import { SetterCallback } from '@typedly/callback';
@@ -7,17 +7,17 @@ import { SetterCallback } from '@typedly/callback';
  * @description The interface defines the structure for a property controller that manages property descriptors and their associated metadata.
  * @export
  * @interface PropertyControllerShape
- * @template {Record<PropertyKey, any>} O 
- * @template {keyof O extends string | symbol ? keyof O : never} [K=keyof O extends string | symbol ? keyof O : never] 
- * @template {boolean} [A=boolean] 
- * @template {boolean} [F=boolean] 
- * @template {boolean} [C=boolean] 
- * @template {boolean} [E=boolean] 
- * @template {WrappedPropertyDescriptor<O, K, A, F, C, E>} [D=WrappedPropertyDescriptor<O, K, A, F, C, E>] 
+ * @template {Record<PropertyKey, any>} O
+ * @template {keyof O} [K=keyof O] The key type of the object O.
+ * @template {boolean} [A=boolean] The type of active state of the property.
+ * @template {boolean} [F=boolean] The type of enabled state of the property.
+ * @template {boolean} [C=boolean] The type of configurable state of the property.
+ * @template {boolean} [E=boolean] The type of enumerable state of the property.
+ * @template {WrappedPropertyDescriptor<O, K, A, F, C, E>} [D=WrappedPropertyDescriptor<O, K, A, F, C, E>]
  */
 export interface PropertyControllerShape<
   O extends Record<PropertyKey, any>,
-  K extends keyof O extends string | symbol ? keyof O : never = keyof O extends string | symbol ? keyof O : never,
+  K extends keyof O = keyof O,
   A extends boolean = boolean,
   F extends boolean = boolean,
   C extends boolean = boolean,
@@ -25,107 +25,128 @@ export interface PropertyControllerShape<
   D extends WrappedPropertyDescriptor<O, K, A, F, C, E> = WrappedPropertyDescriptor<O, K, A, F, C, E>,
 > {
   /**
-   * @description
+   * @description Gets the active state of the property.
    * @readonly
    * @type {(A | undefined)}
    */
   get active(): A | undefined;
 
   /**
-   * @description
+   * @description Gets the current descriptor.
    * @readonly
    * @type {D}
    */
   get descriptor(): D;
 
   /**
-   * @description
+   * @description Gets the descriptor chain for the property.
    * @readonly
-   * @type {(PropertyDescriptorChainShape<O, K, any, A, C, E> | undefined)}
+   * @type {(PropertyDescriptorChainShape<O, K, O[K], A, F, C, E> | undefined)}
    */
-  get descriptorChain(): PropertyDescriptorChainShape<O, K, any, A, C, E> | undefined;
+  get descriptorChain(): PropertyDescriptorChainShape<O, K, O[K], A, F, C, E, D> | undefined;
 
   /**
-   * @description
+   * @description Gets the descriptors for the object properties.
    * @readonly
-   * @type {(Set<PropertyDescriptor> | undefined)}
+   * @type {(DescriptorsShape<O, K, O[K], A, F, C, E, D> | undefined)}
    */
-  get descriptors(): Set<PropertyDescriptor> | undefined;
+  get descriptors(): DescriptorsShape<O, K, O[K], A, F, C, E, D> | undefined;
 
   /**
-   * @description
+   * @description Gets the key of the property.
    * @readonly
    * @type {K}
    */
   get key(): K;
 
   /**
-   * @description
+   * @description Gets the object that the property controller is managing.
    * @readonly
    * @type {O}
    */
   get object(): O;
 
   /**
-   * @description
+   * @description Gets the previous descriptor.
    * @readonly
-   * @type {PropertyDescriptor}
+   * @type {D}
    */
-  get previousDescriptor(): PropertyDescriptor;
+  get previousDescriptor(): D;
 
   /**
-   * @description
+   * @description Gets the private key of the property.
    * @readonly
    * @type {PropertyKey}
    */
   get privateKey(): PropertyKey;
 
   /**
-   * @description
+   * @description Attaches the property controller to the object.
    * @returns {this} 
    */
   attach(): this;
 
-  // Descriptors.
-  addDescriptor(descriptor: PropertyDescriptor): this;
-  getDescriptor(id: number): PropertyDescriptor;
-  removeDescriptor(id: number): this;
+  //#region Descriptors.
+  /**
+   * @description Adds a new descriptor.
+   * @param {D} descriptor 
+   * @returns {this} 
+   */
+  addDescriptor(descriptor: D): this;
 
   /**
-   * @description
+   * @description Gets the descriptor at the specified index.
+   * @param {number} index 
+   * @returns {(D | undefined)} 
+   */
+  getDescriptor(index: number): D | undefined;
+
+  /**
+   * @description Removes the descriptor at the specified index.
+   * @param {number} index 
+   * @returns {this} 
+   */
+  removeDescriptor(index: number): this;
+  //#endregion Descriptors.
+
+  //#region Descriptor.
+  /**
+   * @description Gets the getter function of the descriptor.
    * @type {(D['get'] | undefined)}
    */
   get: D['get'] | undefined;
 
   /**
-   * @description
+   * @description Gets the setter function of the descriptor.
    * @type {(D['set'] | undefined)}
    */
   set: D['set'] | undefined;
 
   /**
-   * @description
+   * @description Current `onGet` callback.
    * @type {(GetterCallback<O, K> | undefined)}
    */
   onGet: GetterCallback<O, K> | undefined;
 
   /**
-   * @description
+   * @description Current `onSet` callback.
    * @type {(SetterCallback<O, K> | undefined)}
    */
   onSet: SetterCallback<O, K> | undefined;
+  //#endregion Descriptor.
 
   // Active.
   /**
-   * @description
+   * @description Checks if the descriptor at index is active.
+   * @param {number} index - The index of the descriptor to check.
    * @returns {boolean} 
    */
-  isActive(): boolean;
+  isActive(index: number): boolean;
 
   /**
-   * @description
+   * @description Sets the active state of the descriptor at index.
    * @param {boolean} active 
    * @returns {this} 
    */
-  setActive(active: boolean): this;
+  setActive(active: boolean, index: number): this;
 }
